d5e310d630934215e266442f21fb10230df29381
Salvatore Previti <salvatorepreviti@improbable.io>
2021-06-02 15:13:12 +0100

    compile, gl-constants

--- scripts/build.cjs
+++ scripts/build.cjs
@@ -1,41 +1,54 @@
+const fs = require('fs')
+const fastglob = require('fast-glob')
 const path = require('path')
 const { forkAsync } = require('./lib/child-processes')
 
 const typescriptCliPath = require.resolve('typescript/lib/tsc')
+const eslintCliPath = require.resolve('eslint/bin/eslint.js')
 
 const ROOT_DIR = path.dirname(__dirname)
 
-async function compileTsPackage(name) {
-  const timeTitle = `Package ${name} compiled.`
-
-  console.time(timeTitle)
-
-  const projectDir = path.resolve(ROOT_DIR, 'packages', name)
-
-  const tscPromises = []
-
+async function compileTsPackage(projectDir) {
+  projectDir = path.resolve(ROOT_DIR, projectDir)
   const tscArgs = []
-  tscArgs.push('-p', path.resolve(projectDir, 'tsconfig.json'))
-  tscArgs.push('--emit')
+  tscArgs.push('-p', path.resolve(projectDir, 'tsconfig.build.json'))
   tscArgs.push('--declaration')
 
   const esmArgs = [...tscArgs]
   esmArgs.push('--outdir', path.join(projectDir, 'dist/esm'))
   esmArgs.push('--module', 'ESNext')
-  tscPromises.push(forkAsync(typescriptCliPath, esmArgs))
+  await forkAsync(typescriptCliPath, esmArgs)
 
   const cjsArgs = [...tscArgs]
   cjsArgs.push('--outdir', path.join(projectDir, 'dist/cjs'))
   cjsArgs.push('--module', 'CommonJS')
-  tscPromises.push(forkAsync(typescriptCliPath, esmArgs))
+  await forkAsync(typescriptCliPath, cjsArgs)
 
-  await Promise.all(tscPromises)
+  const distDir = path.resolve(projectDir, 'dist')
 
-  console.timeEnd(timeTitle)
+  for (const f of await fastglob('cjs/**/*.js', { onlyFiles: true, absolute: true, cwd: distDir })) {
+    fs.renameSync(f, `${f}.cjs`)
+  }
 }
 
 async function main() {
-  await compileTsPackage('gl-constants')
+  console.log('Compiling...')
+
+  for (const projectName of fs.readdirSync(path.resolve(ROOT_DIR, 'packages'))) {
+    const projectDir = path.resolve(ROOT_DIR, 'packages', projectName)
+    if (fs.existsSync(path.resolve(projectDir, 'tsconfig.build.json'))) {
+      const timeTitle = `Package ${projectName} compiled.`
+      console.log('Compiling package', projectName)
+      console.time(timeTitle)
+      await compileTsPackage(projectDir)
+      console.timeEnd(timeTitle)
+    }
+  }
+
+  console.log('Linting...')
+  console.time('Linting completed')
+  await forkAsync(eslintCliPath, ['--fix', '.'], { cwd: ROOT_DIR })
+  console.timeEnd('Linting completed')
 }
 
 main().catch((e) => {
